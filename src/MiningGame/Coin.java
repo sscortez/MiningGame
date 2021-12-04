b91566a9e72e9cc56c703716563930c5b4804e43
Samuel Cortez <ss.cortez92@gmail.com>
2021-12-03 22:51:59 -0600

    Edit coins

--- src/MiningGame/Coin.java
+++ src/MiningGame/Coin.java
@@ -1,14 +1,9 @@
 package MiningGame;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
+ *Class represents the coin
  *
- * @author sscortez16
+ * @author kzavala
  */
 public class Coin {
     
